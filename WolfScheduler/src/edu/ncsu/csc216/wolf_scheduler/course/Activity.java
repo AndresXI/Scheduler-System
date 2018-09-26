0827381ae1c5c408d020ee5f7f8b099f3a761c4c
Andres Alcocer <aalcoce@ncsu.edu>
2018-09-25 21:13:41 -0400

    test3

--- WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/Activity.java
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/Activity.java
@@ -321,7 +321,7 @@ public abstract class Activity implements Conflict {
       throw new ConflictException("The course cannot be added due to a conflict");
     } 
     
-    if (this.getMeetingDays().indexOf(possibleConflictingActivity.getMeetingDays()) > 1 
+    if (this.getMeetingDays().contains(possibleConflictingActivity.getMeetingDays()) 
         && this.getEndTime() > possibleConflictingActivity.getStartTime()) {
         
       throw new ConflictException("Conflicting event"); 
